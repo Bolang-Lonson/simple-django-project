a52a370640847265bb014b71786e471b822fa8e7
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-10-09 12:35:32 +0100

    installed and added the sorl-thumbnail module to the project

--- feed/models.py
+++ feed/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     text = models.CharField(max_length=140, blank=False, null=False)
+    image = models.F
 
     def __str__(self):
         return self.text
