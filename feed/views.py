5a6a15da1f201052646a9d24fd0fea10c88991de
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-10-09 17:47:07 +0100

    added image upload and post functionality

--- feed/views.py
+++ feed/views.py
@@ -1,12 +1,14 @@
-from django.views.generic import TemplateView, DetailView
+from django.http import HttpResponse
+from django.views.generic import TemplateView, DetailView, FormView
 from .models import Post
+from .forms import PostForm
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.all()
+        context['posts'] = Post.objects.all().order_by('-id')
 
         return context
 
@@ -14,3 +16,19 @@ class HomePageView(TemplateView):
 class PostDetailView(DetailView):
     template_name = 'detail.html'
     model = Post
+
+
+class AddPostView(FormView):
+    template_name = 'new_post.html'
+    form_class = PostForm
+    success_url = '/'
+
+    def form_valid(self, form) -> HttpResponse:
+        # print(form.cleaned_data['text'])   printing data from the form
+        #   Creating a new Post
+        new_post = Post.objects.create(
+            text = form.cleaned_data['text'],
+            image = form.cleaned_data['image']
+        )
+
+        return super().form_valid(form)
